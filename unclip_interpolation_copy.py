3ccb5c3f96b22dddfdba331d681ca64540bd86c8
limbicnation <gero@limbicnation.com>
2023-07-29 20:51:32 +0200

    Fix UnCLIPImageVariationPipeline interpolation steps\n\nModify the script to properly control interpolation steps using a loop, as the 'UnCLIPImageVariationPipeline' does not have a direct 'num_steps' parameter. The script now uses the provided 'steps' argument in a loop to achieve the desired number of interpolation steps and saves the output images accordingly. Also, updated the command-line arguments in the argparse to include both 'interpolation_factor' and 'steps'.

--- unclip_interpolation_copy.py
+++ unclip_interpolation_copy.py
@@ -1,44 +1,57 @@
+# Original script by the Hugging Faces Diffusers library
+
+import argparse
 import torch
 import os
-from diffusers import DiffusionPipeline
+from diffusers import UnCLIPImageVariationPipeline
 from PIL import Image
 
-device = torch.device("cpu" if not torch.cuda.is_available() else "cuda")
-dtype = torch.float16 if torch.cuda.is_available() else torch.bfloat16
+def main(interpolation_factor, steps):
+    device = torch.device("cpu" if not torch.cuda.is_available() else "cuda")
+    dtype = torch.float16 if torch.cuda.is_available() else torch.bfloat16
 
-pipe = DiffusionPipeline.from_pretrained(
-    "kakaobrain/karlo-v1-alpha-image-variations",
-    torch_dtype=dtype,
-    custom_pipeline="unclip_image_interpolation",  # use polynomial interpolation
-    max_split_size_mb=256,  # set max_split_size_mb to avoid fragmentation
-    interpolation_factor=0.5,  # adjust the strength of the interpolation
-)
+    # Initialize the pipeline with custom_args
+    pipe = UnCLIPImageVariationPipeline.from_pretrained(
+        "kakaobrain/karlo-v1-alpha-image-variations",
+        torch_dtype=dtype,
+        interpolation_factor=interpolation_factor,
+    )
 
-pipe.to(device)
+    pipe.to(device)
 
-# Define image paths to interpolate
-image_paths = [
-    '/media/ws-ml/linux-drive/linux_projects/ml_projects/images/UnClipInterp/art.jpg',
-    '/media/ws-ml/linux-drive/linux_projects/ml_projects/images/UnClipInterp/bear6.png'
-]
+    # Define local image paths to interpolate
+    image_paths = [
+        '/media/ws-ml/linux-drive/linux_projects/ml_projects/images/UnClipInterp/cyberpunk.png',
+        '/media/ws-ml/linux-drive/linux_projects/ml_projects/images/UnClipInterp/painting.jpeg'
+    ]
 
-# Load images
-images = [Image.open(path) for path in image_paths]
+    # Load images
+    images = [Image.open(path) for path in image_paths]
 
-# Set the seed value for the generator
-seed = 68132
-interpolation_methods = ['nearest', 'lanczos', 'bicubic']
+    # Set the seed value for the generator
+    seed = 68132
 
-generator = torch.Generator(device=device).manual_seed(seed)
+    generator = torch.Generator(device=device).manual_seed(seed)
 
-# set the number of steps here
-for steps in [8]:
-    output = pipe(image=images, steps=steps, generator=generator)
+    # Use the interpolation_factor to control interpolation strength
+    # and use a loop to perform multiple interpolation steps.
+    for i in range(steps):
+        output = pipe(image=images, generator=generator)
 
-    # Create a folder to store the output images
-    if not os.path.exists('output'):
-        os.makedirs('output')
+        # Create a folder to store the output images
+        if not os.path.exists('output'):
+            os.makedirs('output')
 
-    # Save images in the 'output' directory
-    for i, image in enumerate(output.images):
-        image.save(f"output/output_10_{steps}_steps_{i}.jpg")
+        # Save images in the 'output' directory
+        for j, image in enumerate(output.images):
+            image.save(f"output/output_{interpolation_factor}_interp_step_{i}_image_{j}.jpg")
+
+if __name__ == "__main__":
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Interpolation strength and steps for image interpolation.")
+    parser.add_argument("--interpolation_factor", type=float, default=0.5, help="Interpolation strength controlled by interpolation_factor.")
+    parser.add_argument("--steps", type=int, default=8, help="Number of interpolation steps.")
+    args = parser.parse_args()
+
+    # Call the main function with the provided interpolation_factor and steps
+    main(args.interpolation_factor, args.steps)
